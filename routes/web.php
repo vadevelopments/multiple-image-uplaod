8ec39c0662d2daa47b218be80b0717e8ec162c6d
NewBeez001 <aldrinpads92@gmail.com>
2022-06-20 16:14:30 +0800

    add: Upload image controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('photos','App\Http\Controllers\PhotosController@view')->name('photos');
+Route::post('photos/store','App\Http\Controllers\PhotosController@store')->name('photos/store');
